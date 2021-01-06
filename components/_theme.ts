1c2e3938008d796dbbed3dbe45e214e174867823
platane <me@platane.me>
2021-01-06 22:51:03 +0100

    🚀prettier colors

--- components/_theme.ts
+++ components/_theme.ts
@@ -5,17 +5,17 @@ export const backgroundColor = "#f5f5f5";
 export const themeColor = "orange";
 
 const standardColors = {
-  blue: ["#ddf3f5", "#abc2e8", "#b0deff", "#bfcfff"],
-  red: ["#e5707e", "#fa877f", "#ffa5a5"],
-  green: ["#a3ddcb", "#e8e9a1", "#a0c1b8", "#cbe2b0", "#adebbe"],
-  yellow: ["#f4ebc1", "#faf0af", "#fdfdc4"],
+  blue: ["#ddf3f5", "#abc2e8", "#b0deff", "#bfcfff", "#b4daf7"],
+  red: ["#f6acac", "#e5707e", "#fa877f", "#ffa5a5"],
+  green: ["#a3ddcb", "#e8e9a1", "#a0c1b8", "#cbe2b0", "#adebbe", "#b4f7c3"],
+  yellow: ["#f4ebc1", "#faf0af", "#fdfdc4", "#f7f2cb"],
 
   pink: ["#f6def6", "#ffb6b9", "#f3c1c6"],
   brown: ["#c7b198", "#dfd3c3", "#daa592"],
 
   white: ["#f0ece3", "#f7f2e7", "#fcf5ee"],
   gray: ["#cccccc", "#eeeeee", "#dfdfdf"],
-  purple: ["#c3aed6", "#be97dc", "#f8b3eb", "#d38cad"],
+  purple: ["#c3aed6", "#be97dc", "#f8b3eb", "#d38cad", "#d2c5f1"],
   black: ["#5d5b6a", "#758184", "#596e79"],
 } as const;
 
